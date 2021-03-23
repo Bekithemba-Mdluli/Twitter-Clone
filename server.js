d8d4b97772dcd7b58bfb319de82b3daf83ff68ea
Bekithemba Mdluli <bekithemba.mdluli.bm@gmail.com>
2021-03-23 11:47:54 +0200

    Added pug template engine

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const colors = require('colors')
+
+dotenv.config({ path: 'config/config.env'})
+const app = express();
+
+const PORT = process.env.PORT
+
+const server = app.listen(PORT, () => {
+    console.log(`Server Running on port ${PORT}`.yellow)
+})
+
+// Tell express which template to use
+app.set("view engine", "pug")
+app.set("views", "static")
+
+app.get("/", (req, res, next) => {
+    res.status(200).render("home")
+})
